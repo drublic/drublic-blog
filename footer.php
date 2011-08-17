6a71899743d381e99653363389b62543b00032b2
drublic <hans@sa-designz.de>
2011-08-17 22:15:10 +0200

    Update jQuery to 1.6.2
    Signed-off-by: drublic <hans@sa-designz.de>

--- footer.php
+++ footer.php
@@ -29,13 +29,13 @@
   <!-- JavaScript at the bottom for fast page loading -->
 
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php bloginfo( 'template_url' ); ?>/js/libs/jquery-1.6.1.min.js"><\/script>')</script>
 
 
   <!-- scripts concatenated and minified via ant build script-->
-  <script src="<?php bloginfo( 'template_url' ); ?>/js/plugins.js"></script>
-  <script src="<?php bloginfo( 'template_url' ); ?>/js/script.js"></script>
+  <script defer src="<?php bloginfo( 'template_url' ); ?>/js/plugins.js"></script>
+  <script defer src="<?php bloginfo( 'template_url' ); ?>/js/script.js"></script>
   <!-- end scripts-->
 
 
