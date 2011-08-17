e0a6e2c4077fccc01b94b2200d9fd3cf8f1cc74f
drublic <hans@sa-designz.de>
2011-08-17 23:02:45 +0200

    Refactor page
    Signed-off-by: drublic <hans@sa-designz.de>

--- loop-page.php
+++ loop-page.php
@@ -16,21 +16,16 @@
 ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php if ( is_front_page() ) { ?>
+		  <h2><?php the_title(); ?></h2>
+		<?php } else { ?>
+			<h1><?php the_title(); ?></h1>
+		<?php } ?>
 
-				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<?php if ( is_front_page() ) { ?>
-						<h2 class="entry-title"><?php the_title(); ?></h2>
-					<?php } else { ?>
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-					<?php } ?>
-
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
-						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
-					</div><!-- .entry-content -->
-				</div><!-- #post-## -->
-
-				<?php comments_template( '', true ); ?>
+    <?php the_content(); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+		<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+  </article>
 
 <?php endwhile; // end of the loop. ?>
