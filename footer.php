612fa95c22088d2506db79ce85eced27ace2add7
drublic <hans@sa-designz.de>
2011-08-18 00:40:50 +0200

    Fix link for local jQuery
    was still to non-existing version 1.6.1
    
    Signed-off-by: drublic <hans@sa-designz.de>

--- footer.php
+++ footer.php
@@ -30,7 +30,7 @@
 
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="<?php bloginfo( 'template_url' ); ?>/js/libs/jquery-1.6.1.min.js"><\/script>')</script>
+  <script>window.jQuery || document.write('<script src="<?php bloginfo( 'template_url' ); ?>/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
 
 
   <!-- scripts concatenated and minified via ant build script-->
