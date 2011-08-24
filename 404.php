2aa9d4aee52d5909fea332f396e9fe43e603d03e
drublic <hans@sa-designz.de>
2011-08-24 22:13:14 +0200

    Re-factor 404-error-page
    Signed-off-by: drublic <hans@sa-designz.de>

--- 404.php
+++ 404.php
@@ -8,23 +8,19 @@
  */
 
 get_header(); ?>
+  <div id="content" role="main">
 
-	<div id="container">
-		<div id="content" role="main">
-
-			<div id="post-0" class="post error404 not-found">
-				<h1 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h1>
-				<div class="entry-content">
-					<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'twentyten' ); ?></p>
-					<?php get_search_form(); ?>
-				</div><!-- .entry-content -->
-			</div><!-- #post-0 -->
-
-		</div><!-- #content -->
-	</div><!-- #container -->
-	<script type="text/javascript">
+    <section class="error404 not-found">
+		  <h1><?php _e( 'Not Found', 'twentyten' ); ?></h1>
+      <p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'twentyten' ); ?></p>
+			<?php get_search_form(); ?>
+		</section>
+		
+  </div>
+	<script>
 		// focus on search field after it has loaded
 		document.getElementById('s') && document.getElementById('s').focus();
 	</script>
+	<hr>
 
 <?php get_footer(); ?>
