233203ae0c511e664cd8c085da10c79c3aed1771
drublic <hans@sa-designz.de>
2011-10-01 23:43:14 +0200

    Update jQuery
    Signed-off-by: drublic <hans@sa-designz.de>

--- footer.php
+++ footer.php
@@ -30,7 +30,7 @@
   
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
   <?php wp_footer(); ?>
-  <script>window.jQuery || document.write('<script src="<?php bloginfo( 'template_url' ); ?>/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+  <script>window.jQuery || document.write('<script src="<?php bloginfo( 'template_url' ); ?>/js/libs/jquery-1.6.4.min.js"><\/script>')</script>
 
 
   <!-- scripts concatenated and minified via ant build script-->
