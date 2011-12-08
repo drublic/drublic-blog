5e23dac954da3c60a9fc68d8444505443a686140
drublic <hans@sa-designz.de>
2011-12-08 22:13:47 +0100

    Add latest build
    Signed-off-by: drublic <hans@sa-designz.de>

--- publish/footer.php
+++ publish/footer.php
@@ -6,4 +6,34 @@
  * @subpackage drublic-blog
  * @since drublic-blog 1.0
  */
-?> </div> <footer id=footer> <div class="inner clearfix"> <?php get_sidebar( 'footer' ); ?> </div> </footer> </div> <?php wp_footer(); ?> <script>window.jQuery||document.write('<script src="<?php bloginfo( 'template_url' ); ?>/js/libs/jquery-1.6.4.min.js"><\/script>');</script> <script defer src='<?php bloginfo( 'template_url' ); ?>/js/47c5361720cff478aa864fb3adf899052dbca7f2.js'></script> <script defer src="https://apis.google.com/js/plusone.js"></script> <script defer src="http://platform.twitter.com/widgets.js"></script> <script>var pkBaseURL=(("https:"==document.location.protocol)?"https://piwik.drublic.de/":"http://piwik.drublic.de/");document.write('<script src="'+pkBaseURL+'piwik.js"><\/script>');</script><script>try{var piwikTracker=Piwik.getTracker(pkBaseURL+"piwik.php",1);piwikTracker.trackPageView();piwikTracker.enableLinkTracking()}catch(err){};</script><noscript><p><img src="http://piwik.drublic.de/piwik.php?idsite=1" style="border:0" alt=""/></p></noscript> </body> </html>
+?>
+    </div>
+    
+    
+    <footer id=footer>
+      <div class="inner clearfix">
+        <?php get_sidebar( 'footer' ); ?>
+      </div>
+    </footer>
+  </div>
+
+
+  
+  
+  
+  <?php wp_footer(); ?>
+  <script>window.jQuery||document.write('<script src="<?php bloginfo( 'template_url' ); ?>/js/libs/jquery-1.6.4.min.js"><\/script>');</script>
+
+
+  <script defer src='<?php bloginfo( 'template_url' ); ?>/js/47c5361720cff478aa864fb3adf899052dbca7f2.js'></script>
+  
+  
+  <script defer src="https://apis.google.com/js/plusone.js"></script>
+  <script defer src="http://platform.twitter.com/widgets.js"></script>
+
+   
+  <script>var pkBaseURL=(("https:"==document.location.protocol)?"https://piwik.drublic.de/":"http://piwik.drublic.de/");document.write('<script src="'+pkBaseURL+'piwik.js"><\/script>');</script><script>try{var piwikTracker=Piwik.getTracker(pkBaseURL+"piwik.php",1);piwikTracker.trackPageView();piwikTracker.enableLinkTracking()}catch(err){};</script><noscript><p><img src="http://piwik.drublic.de/piwik.php?idsite=1" style="border:0" alt=""/></p></noscript>
+  
+
+</body>
+</html>
