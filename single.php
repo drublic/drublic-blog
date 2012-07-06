b1e024493e5b078bc24820515091ad37984a2db9
Hans Christian Reinl <anything@drublic.de>
2012-07-06 13:14:10 +0200

    Remove disclaimer on "not native"

--- single.php
+++ single.php
@@ -41,14 +41,14 @@ get_header(); ?>
       <?php if (is_linked_list()) : ?>
         <span class="permalink"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink for %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>">&#9776;</a></span>
       <?php endif; ?>
-		  
+
 		  <span class="categories"><?php the_category( ', ' ); ?></span>
 		</header>
-    
+
     <?php if ( get_post_meta( get_the_ID(), 'posts-header', true ) != '' ) : ?>
       <?php print get_post_meta( get_the_ID(), 'posts-header', true ); ?>
     <?php endif; ?>
-    
+
     <?php if ( get_the_post_thumbnail() != "" ) : ?>
       <figure class="aligncenter">
         <?php the_post_thumbnail( 'medium' ); ?>
@@ -65,34 +65,29 @@ get_header(); ?>
         <p>This post is <?php print $oldness; ?> days old. It was written on <?php print get_the_date('d.m.Y'); ?>. Please make sure to be careful with the information provided and check a more recent source on this topic.</p>
       </div>
     <?php endif; ?>
-    
+
     <?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'drublic-blog' ), 'after' => '</div>' ) ); ?>
 
-    <div class="message" id="disclaimer">
-      <p><strong>Disclaimer:</strong> I want to apologize for my grammar and mistakes in this post. I am not a native speaker and try to become better. But hey, please help me: <a href="<?php bloginfo('url'); ?>/contact/">send me a mail</a> or <a href="http://twitter.com/drublic">tweet me</a>. Thank you, you're awesome!</p>
-    </div>
-		
-		
 		<div class="share-post">
       <script type="text/x-handlebars-template" id="share-post">
   		  <a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-text="<?php the_title(); ?>" data-count="horizontal" data-via="drublic" data-related="drublic">Tweet</a>
-  		  
+
   		  <g:plusone size="medium"></g:plusone>
       </script>
 		</div>
-    
+
     <div class="instapaper">
 		  <a href="http://www.instapaper.com/hello2?url=<?php the_permalink(); ?>&amp;title=<?php the_title(); ?>" title="Diesen Artikel später mit Instapaper lesen?" class="button">Instapaper</a>
 		</div>
-		
-		
+
+
 		<p class="tags"><?php the_tags( 'Tagged with: ' ); ?></p>
 	</article>
 	<hr>
-	
+
 	<?php comments_template( '', true ); ?>
-	
+
   <nav id="post-nav">
     <div class="prev"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'drublic-blog' ) . '</span> %title' ); ?></div>
     <div class="next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'drublic-blog' ) . '</span>' ); ?></div>
